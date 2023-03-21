ed63bdbaa3639222e5e7244e477bb7331abfec7b
nhuntera <109431292+nhuntera@users.noreply.github.com>
2023-03-21 15:00:04 -0500

    big one

--- RaycastGame.py
+++ RaycastGame.py
@@ -2,8 +2,56 @@ import DotGL
 import time
 import math
 import world
-
+import keyboard
+player_speed = 0
 level = world.Level("map.txt")
+player_angel = 0
+
+def raycast(dist=5):
+    pass
+def rotate_player(amount):
+
+    player_angel = player_angel + amount
+    if player_angle > 360:
+        player_angle = player_angle - 360 
+    if player_angle < 0:
+        player_angle = 360 - player_angle
+ 
+def player_move(player_speed):
+    
+    new_pos_x = level.player_chords[0] + player_speed*math.cos(player_angel)
+    new_pos_y = level.player_chords[1] + player_speed*math.sin(player_angel)
+
+    level.player_chords[0] = new_pos_x
+    level.player_chords[1] = new_pos_x
+
+def get_keys():
+    try:
+        if keyboard.is_pressed('w'):  # if key 'q' is pressed 
+            player_speed = 1
+        else:
+            player_speed = 0
+        if keyboard.is_pressed('s'):  # if key 'q' is pressed 
+            player_speed = -1
+        else:
+            player_speed = 0
+        if keyboard.is_pressed('d'):  # if key 'q' is pressed 
+            rotate_player(5)
+        if keyboard.is_pressed('a'):  # if key 'q' is pressed 
+            rotate_player(-5)
+    except:
+        pass
 
 #set up the screen
 draw = DotGL.Screen(180, 52, False)
+while True:
+    player_move(player_speed)
+    for n in level.tile_chords:
+        draw.pixel(n[0],n[1])
+    if level.player_chords[0] < 180 and level.player_chords[0] > -1 and level.player_chords[1] < 52 and level.player_chords[1] > -1:
+        draw.pixel(round(level.player_chords[0]),round(level.player_chords[1]),"@")
+    draw.update()
+    time.sleep(0.1)
+    get_keys()
+    print(level.player_chords)
+    print(player_angel)
--- world.py
+++ world.py
@@ -1,14 +1,27 @@
 class Level:
     
     size = 100
-    origin = (0, 0)
+    origin = (22, 9)
+    tiles = []
+    tile_chords = []
+    player_chords = [22,9]
 
     def __init__(self, map_txt_file):
-        map_loader(map_txt_file)
+        self.map_loader(map_txt_file)
         self.level_map = [[False for i in range(self.size)] for j in range(self.size)]
 
     def map_loader(self, map_txt_file):
+        ypos = 0
+        xpos = 0
         f = open(map_txt_file)
         for y in f:
-            level_map.append([])
             for x in y:
+                if x == "#":
+                    self.tiles.append("wall")
+                    self.tile_chords.append((xpos,ypos))
+                xpos = xpos + 1
+            ypos = ypos + 1
+            xpos = 0
+
+level = Level("map.txt")
+print(level.tiles, " ", level.tile_chords)
